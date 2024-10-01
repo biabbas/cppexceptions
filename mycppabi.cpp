60359dc55d833241c49a8d4260f08259c7cd2a0d
B I Mohammed Abbas <bimohammadabbas@gmail.com>
2024-10-01 15:22:18 +0530

    Add cpp abi for catch statements

--- mycppabi.cpp
+++ mycppabi.cpp
@@ -1,10 +1,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <typeinfo>
 
 namespace __cxxabiv1 {
     struct __class_type_info {
-        virtual void fo() {}
+        virtual void foo() {}
     } ti;
 }
 
@@ -15,22 +17,66 @@ extern "C" {
 
 void* __cxa_allocate_exception(size_t thrown_size)
 {
-    printf("alloc ex %li\n", thrown_size);
+    printf("alloc ex %i\n", thrown_size);
     if (thrown_size > EXCEPTION_BUFF_SIZE) printf("Exception too big");
     return &exception_buff;
 }
 
 void __cxa_free_exception(void *thrown_exception);
 
+
 #include <unwind.h>
-void __cxa_throw(
-          void* thrown_exception,
-          struct type_info *tinfo,
-          void (*dest)(void*))
+
+typedef void (*unexpected_handler)(void);
+typedef void (*terminate_handler)(void);
+
+struct __cxa_exception { 
+	std::type_info *	exceptionType;
+	void (*exceptionDestructor) (void *); 
+	unexpected_handler	unexpectedHandler;
+	terminate_handler	terminateHandler;
+	__cxa_exception *	nextException;
+
+	int			handlerCount;
+	int			handlerSwitchValue;
+	const char *		actionRecord;
+	const char *		languageSpecificData;
+	void *			catchTemp;
+	void *			adjustedPtr;
+
+	_Unwind_Exception	unwindHeader;
+};
+
+void __cxa_throw(void* thrown_exception, struct type_info *tinfo, void (*dest)(void*))
 {
-    printf("throw\n");
+    printf("__cxa_throw called\n");
+
+    __cxa_exception *header = ((__cxa_exception *) thrown_exception - 1);
+    _Unwind_RaiseException(&header->unwindHeader);
+
     // __cxa_throw never returns
+    printf("no one handled __cxa_throw, terminate!\n");
     exit(0);
 }
 
-} // extern "C"
+
+void __cxa_begin_catch()
+{
+    printf("begin FTW\n");
+}
+
+void __cxa_end_catch()
+{
+    printf("end FTW\n");
+}
+
+_Unwind_Reason_Code __gxx_personality_v0 (
+                     int version, _Unwind_Action actions, uint64_t exceptionClass,
+                     _Unwind_Exception* unwind_exception, _Unwind_Context* context)
+{
+    printf("Personality function FTW!\n");
+}
+
+
+}
+
