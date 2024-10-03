6b212e4217975316bbd21933d137c6b331454fad
B I Mohammed Abbas <bimohammadabbas@gmail.com>
2024-10-03 17:27:47 +0530

    Part 5: traverse through all entries of cs table

--- mycppabi.cpp
+++ mycppabi.cpp
@@ -70,26 +70,82 @@ void __cxa_end_catch()
     printf("end FTW\n");
 }
 
+
+struct LSDA_Header {
+    uint8_t lsda_start_encoding;
+    uint8_t lsda_type_encoding;
+    uint8_t lsda_call_site_table_length;
+};
+
+struct LSDA_Call_Site_Header {
+    uint8_t encoding;
+    uint8_t length;
+};
+
+struct LSDA_Call_Site {
+    
+    LSDA_Call_Site(const uint8_t *ptr) {
+        cs_start = ptr[0];
+        cs_len = ptr[1];
+        cs_lp = ptr[2];
+        cs_action = ptr[3];
+    }
+
+    uint8_t cs_start;
+    uint8_t cs_len;
+    uint8_t cs_lp;
+    uint8_t cs_action;
+};
+
 _Unwind_Reason_Code __gxx_personality_v0 (
                      int version, _Unwind_Action actions, uint64_t exceptionClass,
                      _Unwind_Exception* unwind_exception, _Unwind_Context* context)
 {
-    if(actions & _UA_SEARCH_PHASE)
+    if (actions & _UA_SEARCH_PHASE)
     {
-        printf("personality function, lookup phase\n");
+        printf("Personality function, lookup phase\n");
         return _URC_HANDLER_FOUND;
-    }
-    else if (actions & _UA_CLEANUP_PHASE)
-    {
-        printf("personality function, cleanup phase\n");
+    } else if (actions & _UA_CLEANUP_PHASE) {
+        printf("Personality function, cleanup\n");
+
+        const uint8_t* lsda = (const uint8_t*)
+                                    _Unwind_GetLanguageSpecificData(context);
+
+        LSDA_Header *header = (LSDA_Header*)(lsda);
+        LSDA_Call_Site_Header *cs_header = (LSDA_Call_Site_Header*)
+                                                (lsda + sizeof(LSDA_Header));
+
+        size_t cs_in_table = cs_header->length / sizeof(LSDA_Call_Site);
+
+        // We must declare cs_table_base as uint8, otherwise we risk an
+        // unaligned access
+        const uint8_t *cs_table_base = lsda + sizeof(LSDA_Header)
+                                            + sizeof(LSDA_Call_Site_Header);
+
+        // Go through every entry on the call site table
+        for (size_t i=0; i < cs_in_table; ++i)
+        {
+            const uint8_t *offset = &cs_table_base[i * sizeof(LSDA_Call_Site)];
+            LSDA_Call_Site cs(offset);
+            printf("Found a CS:\n");
+            printf("\tcs_start: %i\n", cs.cs_start);
+            printf("\tcs_len: %i\n", cs.cs_len);
+            printf("\tcs_lp: %i\n", cs.cs_lp);
+            printf("\tcs_action: %i\n", cs.cs_action);
+        }
+
+        uintptr_t ip = _Unwind_GetIP(context);
+        uintptr_t funcStart = _Unwind_GetRegionStart(context);
+        uintptr_t ipOffset = ip - funcStart;
+
+
         return _URC_INSTALL_CONTEXT;
-    }
-    else
-    {
-        printf("personality function, fatal error\n");
+    } else {
+        printf("Personality function, error\n");
         return _URC_FATAL_PHASE1_ERROR;
     }
 }
+
 
 
 }
--- throw.cpp
+++ throw.cpp
@@ -1,35 +1,30 @@
-#include "throw.h"
 #include <stdio.h>
+#include "throw.h"
 
-// Notice we're adding a second exception type
 struct Fake_Exception {};
 
 void raise() {
     throw Exception();
 }
 
-// We will analyze what happens if a try block doesn't catch an exception
 void try_but_dont_catch() {
     try {
         raise();
     } catch(Fake_Exception&) {
-        printf("Running try_but_dont_catch::catch(Fake_Exception)\n");
+        printf("Caught a Fake_Exception!\n");
     }
 
-    printf("try_but_dont_catch handled an exception and resumed execution\n");
+    printf("try_but_dont_catch handled the exception\n");
 }
 
-// And also what happens when it does
 void catchit() {
     try {
         try_but_dont_catch();
     } catch(Exception&) {
-        printf("Running try_but_dont_catch::catch(Exception)\n");
-    } catch(Fake_Exception&) {
-        printf("Running try_but_dont_catch::catch(Fake_Exception)\n");
+        printf("Caught an Exception!\n");
     }
 
-    printf("catchit handled an exception and resumed execution\n");
+    printf("catchit handled the exception\n");
 }
 
 extern "C" {
